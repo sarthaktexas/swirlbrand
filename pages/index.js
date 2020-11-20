f46afe69e6469d65d921270c3ac6572d44e83d29
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-20 09:35:03 +0000

    🎨 structure now creates page without z-{n}

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,34 @@
 import Nav from '../components/nav'
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
       <img
         src="/checkerboard-graphic.png"
         alt="Black and White Checkerboard"
         width={1000}
-        className="pointer-events-none z-0 -mt-64 md:-ml-8 absolute bottom-0"
+        className="pointer-events-none -mt-64 sm:-ml-8 absolute bottom-0"
       />
       <img
         src="/homepage-picture.png"
         alt="Picture of person wearing sweatshirt"
         // width={1000}
-        className="pointer-events-none z-0 -mt-64 md:-ml-8 absolute bottom-0 w-96 md:w-64"
+        className="pointer-events-none -mt-64 sm:-ml-8 absolute bottom-0 w-96 sm:w-64"
       />
-      <div className="z-10 p-20 ">
+      <Nav/>
+      <div className="absolute p-20 mt-24 md:mt-0">
         {/* <h3 className="text-3xl text-black font-serif font-extralight uppercase dark:text-white">
           Pure.
         </h3> */}
-        <h1 className="text-6xl text-black font-corben font-bold dark:text-white">
+        <h1 className="pointer-events-none outline-text text-6xl text-black font-corben font-bold dark:text-purple-500">
           swirl.
         </h1>
         <Link href="/latest-collection">
-          <p className="align-center md:w-56 font-varela font-bold text-black p-2 border-2 md:hover:border-purple-600 rounded hover:text-white hover:bg-black">
-            Shop our latest collection
+          <p className="bg-white align-center sm:w-56 font-varela font-bold text-black p-2 border-2 border-black sm:hover:border-purple-600 rounded hover:text-white hover:bg-black">
+            <div className="pointer-events-none">
+              Shop our latest collection
+            </div>
           </p>
         </Link>
       </div>
