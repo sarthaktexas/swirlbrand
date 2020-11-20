b13f3b6544f2a00a9e0596204d78e9a1df5a7e0c
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-20 08:14:43 +0000

    ✨ main: nav bar is row on sm and col on md+

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,18 @@ export default function IndexPage() {
   return (
     <div>
       <Nav />
-      {/* <img
+      <img
+        src="/checkerboard-graphic.png"
+        alt="Black and White Checkerboard"
+        width={1000}
+        className="z-0 -mt-64 md:-ml-8 absolute bottom-0"
+      />
+      <img
         src="/homepage-picture.png"
         alt="Picture of person wearing sweatshirt"
-        width={800}
-        className="z-0 -ml-10 absolute bottom-0"
-      /> */}
+        // width={1000}
+        className="z-0 -mt-64 md:-ml-8 absolute bottom-0 w-96 md:w-64"
+      />
       <div className="z-10 p-20 ">
         {/* <h3 className="text-3xl text-black font-serif font-extralight uppercase dark:text-white">
           Pure.
