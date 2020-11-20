cc4e724c2f956128fca65c844b4b70a42a240f75
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-20 09:34:27 +0000

    🐛 fix nav selection

--- components/nav.js
+++ components/nav.js
@@ -12,12 +12,14 @@ export default function Nav() {
   return (
     <nav>
       {/* <ul className="flex justify-between items-center p-4 blur"> */}
-        <ul className="flex flex-wrap mx-auto md:flex-col float-right mt-2 mr-2 md:mr-5">
+        <ul className="flex flex-wrap mx-auto sm:flex-col float-right mt-2 mr-2 md:mr-5">
           {links.map(({ href, label }) => (
             <li key={`${href}${label}`} className="pb-5">
               <Link href={href}>
-                <p className="text-xl md:text-3xl text-black dark:text-white font-spectral font-black no-underline border-dashed border-b-4 border-white px-2 transform uppercase hover:scale-110 hover:border-black transition duration-500">
-                  {label}
+                <p className={`text-xl md:text-3xl text-black dark:text-white font-spectral font-black no-underline border-dashed border-b-4 border-swirl-orange px-2 transform uppercase hover:scale-110 transition duration-500`}>
+                  <div className="pointer-events-none">
+                    {label}
+                  </div>
                 </p>
               </Link>
             </li>
