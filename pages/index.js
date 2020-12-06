203977e5a1fea98ab1b2778d23f421a8fe04c963
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-12-06 19:29:58 +0000

    💄 update some styles

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,13 @@ export default function IndexPage() {
       <Head>
         <script async defer data-website-id="bb8c0f29-bac3-4894-be39-e025a429ac79" src="https://analytics.sarthakmohanty.me/umami.js"></script>
       </Head>
-      <h3 className="text-left font-corben font-bold text-3xl">Swirl</h3>
-      <h3 className="text-left font-mytupi uppercase">Shop our latest collection.</h3>
-      <h3 className="text-left font-mytupi uppercase">Tops</h3>
-      <h3 className="text-left font-mytupi uppercase">Bottoms</h3>
-      <h3 className="text-left font-mytupi uppercase">Sweatshirts</h3>
-      <h3 className="text-left font-mytupi uppercase">Stickers</h3>
-      <h3 className="text-left font-mytupi uppercase">About</h3>
+      <h3 className="text-left font-corben font-bold text-6xl">Swirl</h3>
+      <h3 className="text-left font-mytupi font-bold uppercase">Shop our latest collection.</h3>
+      <h3 className="text-left font-mytupi font-bold uppercase">Tops</h3>
+      <h3 className="text-left font-mytupi font-bold uppercase">Bottoms</h3>
+      <h3 className="text-left font-mytupi font-bold uppercase">Sweatshirts</h3>
+      <h3 className="text-left font-mytupi font-bold uppercase">Stickers</h3>
+      <h3 className="text-left font-mytupi font-bold uppercase">About</h3>
       <h3></h3>
     </div>
   )
